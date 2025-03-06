296f67d72cd584f903b959aa7e1019768681113b
NycollyWeiss <nycollyweissoliveira@gmail.com>
2025-03-06 17:30:29 -0300

    alteracao no metodo put do PostagemController

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,4 +1,5 @@
 package com.generation.blogpessoal.controller;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +22,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -32,7 +34,8 @@ public class PostagemController {
 	
 @Autowired
 private PostagemRepository postagemRepository;
-
+@Autowired
+private TemaRepository temaRepository;
 
 @GetMapping
 public ResponseEntity <List<Postagem>> getAll(){
@@ -64,15 +67,25 @@ public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
 
 @PostMapping()
 public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+	if(temaRepository.existsById(postagem.getTema().getId()))
 	return ResponseEntity.status(HttpStatus.CREATED)
 			.body(postagemRepository.save(postagem));
+	
+	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!" , null); 
 }
 
 @PutMapping()
 public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-	return postagemRepository.findById(postagem.getId()).map(resposta -> ResponseEntity.status(HttpStatus.OK)
-			.body(postagemRepository.save(postagem)))
-			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	if(postagemRepository.existsById(postagem.getId())) {
+		
+		if(temaRepository.existsById(postagem.getTema().getId()))
+			return  ResponseEntity.status(HttpStatus.OK)
+					.body(postagemRepository.save(postagem));
+	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!" , null); 
+	
+	}	
+		
+	return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 }
 
 
